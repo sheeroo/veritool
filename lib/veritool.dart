7c8c8a9a3c91ec0a7196323208dcc9b1aebdfcd7
George Sirogiannis <sirogiannisgiw@gmail.com>
2023-04-13 14:59:04 +0300

    🔥 init

---
+++ lib/veritool.dart
@@ -0,0 +1,10 @@
+/// veritool, Veritool is a minimalistic Dart CLI for Semantic Versioning-based version number updates in Flutter app&#x27;s pubspec.yaml file.
+///
+/// ```sh
+/// # activate veritool
+/// dart pub global activate veritool
+///
+/// # see usage
+/// veritool --help
+/// ```
+library veritool;
